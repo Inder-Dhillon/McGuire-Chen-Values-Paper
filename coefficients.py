1339a5c02d63aa196ac886c4515d1b81cd37b08b
Inderpal Singh Dhillon <inder_dhillon@outlook.com>
2019-07-14 20:48:13 -0400

    Started work on GUI

--- coefficients.py
+++ coefficients.py
@@ -1,11 +1,16 @@
 import pandas as pd
 import numpy as np
+from tkinter import *
+
+root = Tk()
+root.title("Values")
+root.geometry("600x800")
+
 
 result_list = ['w1', 'w2', 'w3', 'w4', 'w5', 'f12', 'f13', 'f14', 'f15', 'f23', 'f24', 'f25', 'f34', 'f35', 'f45']
 toggle_chen = None
 toggle_mcg = None
-z = int(input("Enter variable: "))
-mode = input("Enter c for chen values, m for mcguire or cm for both: ")
+mode = "cm"
 
 #Importing Chen Values
 chen_67_to_69 = pd.read_csv('chcof1.csv')
@@ -30,38 +35,54 @@ mcg_70_to_73.index = result_list
 mcg_74_to_76.index = result_list
 mcg_77_to_90.index = result_list
 
-if mode == "m" or mode =="cm":
-    if z>=57 and z <= 59:
-        toggle_mcg = mcg_57_to_59
-    elif z>=60 and z <= 69:
-        toggle_mcg = mcg_60_to_69
-    elif z>=70 and z <= 73:
-        toggle_mcg = mcg_70_to_73
-    elif z>=74 and z <= 76:
-        toggle_mcg = mcg_74_to_76
-    elif z>=77 and z <= 90:
-        toggle_mcg = mcg_77_to_90
-    else:
-        raise Exception("Atomic number not in supported range.")
-    print("****McG Values****")
-    for i in range(-2, 3):
-        toggle_mcg["a" + str(i)] = toggle_mcg["a" + str(i)] * (z ** i)
-    print(np.exp(toggle_mcg.sum(axis=1)).round(5))
 
-if mode == "c" or mode =="cm" :
-    if z >= 67 and z <= 69:
-        toggle_chen = chen_67_to_69.copy()
-    elif z >= 70 and z <= 74:
-        toggle_chen = chen_70_to_74.copy()
-    elif z >= 75 and z <= 80:
-        toggle_chen = chen_75_to_80.copy()
-    elif z >= 81 and z <= 85:
-        toggle_chen = chen_81_to_85.copy()
-    elif z >= 86 and z <= 90:
-        toggle_chen = chen_86_to_90.copy()
-    else:
-        raise Exception("Atomic number not in supported range.")
-    print("****Chen Values****")
-    for i in range(-2, 3):
-        toggle_chen["a" + str(i)] = toggle_chen["a" + str(i)] * (z ** i)
-    print(np.exp(toggle_chen.sum(axis=1)).round(5))
+def computation(event):
+    z = int(element.get())
+    if mode == "m" or mode =="cm":
+        if z>=57 and z <= 59:
+            toggle_mcg = mcg_57_to_59
+        elif z>=60 and z <= 69:
+            toggle_mcg = mcg_60_to_69
+        elif z>=70 and z <= 73:
+            toggle_mcg = mcg_70_to_73
+        elif z>=74 and z <= 76:
+            toggle_mcg = mcg_74_to_76
+        elif z>=77 and z <= 90:
+            toggle_mcg = mcg_77_to_90
+        else:
+            raise Exception("Atomic number not in supported range.")
+        print("****McG Values****")
+        for i in range(-2, 3):
+            toggle_mcg["a" + str(i)] = toggle_mcg["a" + str(i)] * (z ** i)
+        mcg_results["text"]= (np.exp(toggle_mcg.sum(axis=1)).round(5))
+
+    if mode == "c" or mode =="cm" :
+        if z >= 67 and z <= 69:
+            toggle_chen = chen_67_to_69.copy()
+        elif z >= 70 and z <= 74:
+            toggle_chen = chen_70_to_74.copy()
+        elif z >= 75 and z <= 80:
+            toggle_chen = chen_75_to_80.copy()
+        elif z >= 81 and z <= 85:
+            toggle_chen = chen_81_to_85.copy()
+        elif z >= 86 and z <= 90:
+            toggle_chen = chen_86_to_90.copy()
+        else:
+            raise Exception("Atomic number not in supported range.")
+        print("****Chen Values****")
+        for i in range(-2, 3):
+            toggle_chen["a" + str(i)] = toggle_chen["a" + str(i)] * (z ** i)
+        chen_results["text"]=(np.exp(toggle_chen.sum(axis=1)).round(5))
+
+
+root["bg"] = "white"
+Label(root, text="Element:", font="Roboto", bg="white").pack(pady=7)
+element = Entry(root, justify=CENTER, bg="#dcdcdc", width=80, borderwidth=0)
+element.pack()
+mcg_results = Label(root, text="", font=("Roboto", 13), bg="white")
+mcg_results.pack(padx=1)
+chen_results = Label(root, text="", font=("Roboto", 13), bg="white")
+chen_results.pack(padx=1)
+
+element.bind("<Return>", computation)
+root.mainloop()
