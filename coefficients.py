d47ab88a06b4b8e991a258e2a0c594e85d8c4f30
Inderpal Singh Dhillon <inder_dhillon@outlook.com>
2019-07-18 13:09:41 -0400

    First Build
    Fully tested for end cases and error handling. Code not clean and efficient yet.

--- coefficients.py
+++ coefficients.py
@@ -2,37 +2,53 @@ import pandas as pd
 import numpy as np
 from tkinter import *
 
+toggle_chen = None
+toggle_mcg = None
+chen_67_to_69 = pd.read_csv('chcof1.id', index_col=0)
+chen_70_to_74 = pd.read_csv('chcof2.id', index_col=0)
+chen_75_to_80 = pd.read_csv('chcof3.id', index_col=0)
+chen_81_to_85 = pd.read_csv('chcof4.id', index_col=0)
+chen_86_to_90 = pd.read_csv('chcof5.id', index_col=0)
+# Importing McGuire Values
+mcg_57_to_59 = pd.read_csv('mgcof1.id', index_col=0)
+mcg_60_to_69 = pd.read_csv('mgcof2.id', index_col=0)
+mcg_70_to_73 = pd.read_csv('mgcof3.id', index_col=0)
+mcg_74_to_76 = pd.read_csv('mgcof4.id', index_col=0)
+mcg_77_to_90 = pd.read_csv('mgcof5.id', index_col=0)
+
+def popuperror(msg):
+    popup = Tk()
+    popup.geometry("300x100")
+    popup["bg"] = "white"
+    popup.wm_title("Error")
+    label = Label(popup, text=msg, font="Roboto", bg="white")
+    label.pack(side="top", fill="x", pady=10)
+    B1 = Button(popup, text="Okay", command=popup.destroy, bg="#dcdcdc", borderwidth=0)
+    B1.pack()
+    popup.mainloop()
+
 def computation(event):
-    toggle_chen = None
-    toggle_mcg = None
-    chen_67_to_69 = pd.read_csv('chcof1.csv',index_col=0)
-    chen_70_to_74 = pd.read_csv('chcof2.csv',index_col=0)
-    chen_75_to_80 = pd.read_csv('chcof3.csv',index_col=0)
-    chen_81_to_85 = pd.read_csv('chcof4.csv',index_col=0)
-    chen_86_to_90 = pd.read_csv('chcof5.csv',index_col=0)
-    # Importing McGuire Values
-    mcg_57_to_59 = pd.read_csv('mgcof1.csv',index_col=0)
-    mcg_60_to_69 = pd.read_csv('mgcof2.csv',index_col=0)
-    mcg_70_to_73 = pd.read_csv('mgcof3.csv',index_col=0)
-    mcg_74_to_76 = pd.read_csv('mgcof4.csv',index_col=0)
-    mcg_77_to_90 = pd.read_csv('mgcof5.csv',index_col=0)
-    z = int(element.get())
+    value_entered = element.get()
+    if value_entered.isdigit():
+        z = int(value_entered)
+    else:
+        popuperror("Enter numbers only.")
+
     mode = str(radio_switch.get())
 
     if mode == "m" or mode =="cm":
         if z>=57 and z <= 59:
-            toggle_mcg = mcg_57_to_59
+            toggle_mcg = mcg_57_to_59.copy()
         elif z>=60 and z <= 69:
-            toggle_mcg = mcg_60_to_69
+            toggle_mcg = mcg_60_to_69.copy()
         elif z>=70 and z <= 73:
-            toggle_mcg = mcg_70_to_73
+            toggle_mcg = mcg_70_to_73.copy()
         elif z>=74 and z <= 76:
-            toggle_mcg = mcg_74_to_76
+            toggle_mcg = mcg_74_to_76.copy()
         elif z>=77 and z <= 90:
-            toggle_mcg = mcg_77_to_90
+            toggle_mcg = mcg_77_to_90.copy()
         else:
-            raise Exception("Atomic number not in supported range.")
-        print("****McG Values****")
+            popuperror("Atomic number not in supported range.")
         for i in range(-2, 3):
             toggle_mcg["a" + str(i)] = toggle_mcg["a" + str(i)] * (z ** i)
 
@@ -48,26 +64,40 @@ def computation(event):
         elif z >= 86 and z <= 90:
             toggle_chen = chen_86_to_90.copy()
         else:
-            raise Exception("Atomic number not in supported range.")
-        print("****Chen Values****")
+            popuperror("Atomic number not in supported range.")
         for i in range(-2, 3):
             toggle_chen["a" + str(i)] = toggle_chen["a" + str(i)] * (z ** i)
     if mode == "cm":
+        mcg_label["text"] = "McGuire Results"
+        mcg_label.pack(padx=1)
         mcg_results["text"] = (np.exp(toggle_mcg.sum(axis=1)).round(5))
+        mcg_results.pack(padx=1)
+        chen_label["text"] = "Chen Results"
+        chen_label.pack(padx=1)
         chen_results["text"] = (np.exp(toggle_chen.sum(axis=1)).round(5))
+        chen_results.pack(padx=1)
     elif mode=="c":
+        chen_label["text"] = "Chen Results"
+        chen_label.pack(padx=1)
         chen_results["text"] = (np.exp(toggle_chen.sum(axis=1)).round(5))
-        mcg_results["text"] = ""
+        chen_results.pack(padx=1)
+        mcg_results.pack_forget()
+        mcg_label.pack_forget()
     elif mode =="m":
+        mcg_label["text"] = "McGuire Results"
+        mcg_label.pack(padx=1)
         mcg_results["text"] = (np.exp(toggle_mcg.sum(axis=1)).round(5))
-        chen_results["text"] = ""
+        mcg_results.pack(padx=1)
+        chen_results.pack_forget()
+        chen_label.pack_forget()
+
 
 root = Tk()
 root.title("Values")
 root.geometry("600x800")
 radio_switch = StringVar(None, "cm")
 root["bg"] = "white"
-Label(root, text="Element:", font="Roboto", bg="white").pack(pady=7)
+Label(root, text="Enter Atomic Number:", font="Roboto", bg="white").pack(pady=7)
 element = Entry(root, justify=CENTER, bg="#dcdcdc", width=80, borderwidth=0)
 element.pack()
 Radiobutton(root,
@@ -85,10 +115,9 @@ Radiobutton(root,
               padx = 20,
               variable=radio_switch,
               value="cm", bg="white").pack()
+mcg_label = Label(root, text="", font=("Roboto", 13), bg="white")
 mcg_results = Label(root, text="", font=("Roboto", 13), bg="white")
-mcg_results.pack(padx=1)
+chen_label = Label(root, text="", font=("Roboto", 13), bg="white")
 chen_results = Label(root, text="", font=("Roboto", 13), bg="white")
-chen_results.pack(padx=1)
-
 element.bind("<Return>", computation)
 root.mainloop()
